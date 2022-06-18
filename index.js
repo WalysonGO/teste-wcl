c7ad6542b4354e801a8a57aff7ceca8f101aca46
WalysonGO <walysonoliveira143@gmail.com>
2022-06-18 17:47:40 -0300

    chore: adding search by cep brazil

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 const express = require('express');
 const app = express();
+const axios = require('axios')
 const port = 8090;
 
 // req = Require = é o que o usuario manda para o servidor
 // res = Response = é a resposta do servidor
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send("<h1>Hello World! =)</h1>")
 })
 
+app.post('/:cep', async(req, res) => {
+    const { cep }  = req.params;
+    const { data } = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+    const dataResult = {
+        data
+    }
+    return res.send(dataResult);
+})
+
+app.post('/', async(req, res) => {
+    const { cep }  = req.body;
+    const { data } = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+    const dataResult = {
+        data
+    }
+    return res.send(dataResult);
+})
+
+
 // arrow function () => {}
 app.listen(port, () => {
     console.log(`http://localhost:${port}`);
     console.log("Este servidor está rodando na porta: ", port);
-});
+})
